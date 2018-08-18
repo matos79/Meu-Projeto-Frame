608e183ee9a6c5e13cf1c35a149815af3aabc06e
Eliane <lilicaoj@yahoo.com.br>
2018-08-17 21:42:33 -0300

    meu primeiro envio2

--- application/config/database.php
+++ application/config/database.php
@@ -75,10 +75,10 @@ $query_builder = TRUE;
 
 $db['default'] = array(
 	'dsn'	=> '',
-	'hostname' => 'localhost',
-	'username' => 'root',
-	'password' => '',
-	'database' => 'contatos',
+	'hostname' => 'mysql556.umbler.com:41890',
+	'username' => 'sergiomatos',
+	'password' => 'nicolas040815',
+	'database' => 'frameprojeto',
 	'dbdriver' => 'mysqli',
 	'dbprefix' => '',
 	'pconnect' => FALSE,
